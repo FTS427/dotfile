27a160874133475295665f34a932ce1fc01572af
FTS427 <FTS427@outlook.com>
2024-10-20 12:52:48 +0800

    U | update powermenu

--- useful_script/wofi-poermenu.sh
+++ useful_script/wofi-poermenu.sh
@@ -11,7 +11,7 @@ STYLE="$HOME/.config/wofi/style.css"
 ## Wofi Command
 wofi_command="wofi --show dmenu \
 			--conf ${CONFIG} --style ${STYLE} \
-			--width=300 --height=250 \
+			--width=300 --height=200 \
 			--cache-file=/dev/null \
 			--hide-scroll --no-actions \
 			--define=matching=fuzzy"
@@ -21,23 +21,16 @@ uptime=$(uptime -p | sed -e 's/up //g')
 ## Entries
 shutdown=" Shutdown"
 reboot=" Reboot"
-lock=" Lock"
 suspend=" Sleep"
 logout=" Logout"
 
-# Ask for confirmation
-cdialog () {
-	yad --title='Confirm?' --borders=15 --center --fixed --undecorated --button=Yes:0 --button=No:1 --text="Are you sure?" --text-align=center
-}
-
 # Variable passed to rofi
 open_menu () {
-	options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+	options="$suspend\n$logout\n$reboot\n$shutdown"
 
-	chosen="$(echo -e "$options" | $wofi_command --prompt "UP - $uptime")"
+	chosen="$(echo -e "$options" | $wofi_command --prompt "$uptime")"
 	case $chosen in
 		$shutdown)
-			cdialog
 			if [[ "$?" == 0 ]]; then
 				systemctl poweroff
 			else
@@ -45,29 +38,23 @@ open_menu () {
 			fi
 			;;
 		$reboot)
-			cdialog
 			if [[ "$?" == 0 ]]; then
 				systemctl reboot
 			else
 				exit
 			fi
 			;;
-		$lock)
-			swaylock
-			;;
 		$suspend)
-			cdialog
 			if [[ "$?" == 0 ]]; then
 				mpc -q pause
 				pulsemixer --mute
-				~/.config/hypr/scripts/lockscreen
+				#~/.config/hypr/scripts/lockscreen
 				systemctl suspend
 			else
 				exit
 			fi
 			;;
 		$logout)
-			cdialog
 			if [[ "$?" == 0 ]]; then
 				hyprctl dispatch exit 0
 			else
